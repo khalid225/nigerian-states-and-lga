0d74a420f71a2bcd19b12437183a2c8e2759e0bb
khalid225 <msart4real@yahoo.com>
2023-12-20 15:27:53 +0100

    changed the endpoint to get local government so it can find states either by name or by id

--- routes/index.js
+++ routes/index.js
@@ -17,17 +17,17 @@ router.get("/", (req, res) => {
     });
 });
 
-// API endpoint to get local governments for a given state
-router.get("/get-local-governments/:name", (req, res) => {
+/* // API endpoint to get local governments for a given state
+router.get("/get-local-governments/:id", (req, res) => {
   //get the local government by the state id
-  /* const stateId = req.params.id;
+  const stateId = req.params.id;
 
   State.findOne({ "items.states.id": stateId })
     .then((state) => {
       if (state) {
         const localGovernments = state.items.states
-        .find((s) => s.id === stateId)
-        .locals.map((local) => local.name);
+          .find((s) => s.id === parseInt(stateId))
+          .locals.map((local) => local.name);
         res.json({ localGovernments });
       } else {
         res.status(404).json({ error: "State not found" });
@@ -35,7 +35,7 @@ router.get("/get-local-governments/:name", (req, res) => {
     })
     .catch((error) => {
       res.status(500).json({ error: error.message });
-    }); */
+    });
 
   //get the local government by the state name
   const stateName = req.params.name;
@@ -45,6 +45,39 @@ router.get("/get-local-governments/:name", (req, res) => {
       if (state) {
         const localGovernments = state.items.states
           .find((s) => s.name === stateName)
+          .locals.map((local) => local.name);
+        res.json({ localGovernments });
+      } else {
+        res.status(404).json({ error: "State not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+}); */
+
+// API endpoint to get local governments for a given state by name or id
+router.get("/get-local-governments/:param", (req, res) => {
+  const param = req.params.param;
+  const isParamNumeric = !isNaN(param);
+
+  let query;
+  if (isParamNumeric) {
+    // If param is a number, search by id
+    query = { "items.states.id": parseInt(param) };
+  } else {
+    // If param is a string, search by name
+    query = { "items.states.name": param };
+  }
+  //find the particular state by its given parameter
+  State.findOne(query)
+    .then((state) => {
+      if (state) {
+        //find the array of local governments based on the found state
+        const localGovernments = state.items.states
+          .find((s) =>
+            isParamNumeric ? s.id === parseInt(param) : s.name === param
+          )
           .locals.map((local) => local.name);
         res.json({ localGovernments });
       } else {
